27c8e2302ab53e6d8aee7fed93344954256509e9
Victor <victor.meyo@student.moringaschool.com>
2025-06-30 12:38:38 +0300

    Additional comments added

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,9 +1,11 @@
-#!/usr/bin/env python3
+# 1. Fucntion that returns a list of all the even numbers.
 
 def return_evens(num_list):
-    return [num for num in num_list if num % 2 == 0]
-    pass
-
+    return [num for num in num_list if num % 2 == 0] # Use quotient to check on the remainder so as to check even numbers
+    
+# 2. Function that returns a list of sentence strings with exclamation marks
 def make_exclamation(sentence_list):
-    return [sentence + "!" for sentence in sentence_list]
-    pass
+    return [sentence + "!" for sentence in sentence_list] # Concatenate the 1st portion of sentence with an '!'
+
+
+    
